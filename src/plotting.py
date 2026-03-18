33d91ff2d929cdbba3cdc0a135778534ee42d3d1
Jason Chadwick <jchadwick100@gmail.com>
2026-03-17 22:14:38 -0500

    tweak plotting

--- src/plotting.py
+++ src/plotting.py
@@ -32,20 +32,20 @@ def plot_device_snapshot(
     if ax is None:
         fig,ax = plt.subplots(figsize=(6,6))
     ax.clear()
-    ax.add_patch(mpl.patches.Rectangle((-1.05, -1.05), dev.w+0.1, dev.h+0.1, facecolor='lightgray', zorder=0.5))
+    ax.add_patch(mpl.patches.Rectangle((-1.05, -1.05), dev.w+0.1, dev.h+0.1, facecolor=interpolate('C0', 'w', 0.6), zorder=0.3))
     # Draw device
     for x in range(dev.w):
         for y in range(dev.h):
-            ax.add_patch(mpl.patches.Rectangle((x-1+0.05, y-1+0.05), 0.9, 0.9, facecolor='w', edgecolor='none', zorder=0.5))
+            ax.add_patch(mpl.patches.Rectangle((x-1+0.05, y-1+0.05), 0.9, 0.9, facecolor='w', edgecolor='none', zorder=0.4))
             if x < dev.w and y < dev.h:
-                ax.add_patch(mpl.patches.Rectangle((x-0.5, y-0.25), 0.2, 0.2, facecolor='C2', edgecolor='none', zorder=0.5))
+                ax.add_patch(mpl.patches.Rectangle((x-0.5, y-0.25), 0.2, 0.2, facecolor='C5', edgecolor='none', zorder=0.5))
                 ax.add_patch(mpl.patches.Rectangle((x-0.25, y-0.45), 0.2, 0.4, facecolor='C3', edgecolor='none', zorder=0.5))
     
     for q,coords in frame.qubit_positions.items():
         color = get_q_color(q, code)
         fc = interpolate(color, 'w', 0)
         color = interpolate(color, 'k', 0.2)
-        ax.add_patch(mpl.patches.Circle(coords, radius=0.1, facecolor=fc, edgecolor=color, linewidth=1, zorder=2))
+        ax.add_patch(mpl.patches.Circle(coords, radius=0.15, facecolor=fc, edgecolor=color, linewidth=1, zorder=2))
 
     seen_coords = set()
     for qbs in frame.twoq_gates:
@@ -54,7 +54,7 @@ def plot_device_snapshot(
         y = int(round(coords[1]))
         coords = (x,y)
         if coords not in seen_coords:
-            ax.add_patch(mpl.patches.Rectangle((x-0.25, y-0.45), 0.2, 0.4, edgecolor='limegreen', facecolor='none', linewidth=2, zorder=3))
+            ax.add_patch(mpl.patches.Rectangle((x-0.25, y-0.45), 0.2, 0.4, edgecolor='C2', facecolor='none', linewidth=2, zorder=3))
         seen_coords.add(coords)
 
     ax.set_xlim(-2, dev.w)
@@ -82,6 +82,17 @@ def plot_transition(
     
     ax = plot_device_snapshot(dev, code, frames[-1], ax)
 
+    seen_coords = set()
+    for frame in frames:
+        for qbs in frame.twoq_gates:
+            coords = frame.qubit_positions[qbs[0]]
+            x = int(round(coords[0]))
+            y = int(round(coords[1]))
+            coords = (x,y)
+            if coords not in seen_coords:
+                ax.add_patch(mpl.patches.Rectangle((x-0.25, y-0.45), 0.2, 0.4, edgecolor='C2', facecolor='none', linewidth=2, zorder=3))
+            seen_coords.add(coords)
+
     if not highlight_qubits:
         highlight_qubits = list(qubit_loc_history.keys())
 
@@ -98,7 +109,12 @@ def plot_transition(
         pos2 = positions[-1]
         ax.arrow(pos[0], pos[1], pos2[0]-pos[0], pos2[1]-pos[1], color='w', head_width=0.15, head_length=0.095, length_includes_head=True, linewidth=3, zorder=2.4)
         ax.arrow(pos[0], pos[1], pos2[0]-pos[0], pos2[1]-pos[1], color=color, head_width=0.13, head_length=0.09, length_includes_head=True, linewidth=2, zorder=2.5)
-        # ax.annotate('', xytext=pos, xy=pos2, arrowprops=dict(arrowstyle='->, head_width=0.5, head_length=0.35', linewidth=2, color=color), zorder=2.5)
+        # ax.annotate('', xytext=pos, xy=pos2, arrowprops=dict(arrowstyle='->,
+        # head_width=0.5, head_length=0.35', linewidth=2, color=color),
+        # zorder=2.5)
+    
+    ax.set_title(f't={frames[0].t} to {frames[-1].t} (dt={frames[-1].t-frames[0].t})')
+    return ax
 
 def animate_device(
         dev: device.UnitCellDevice,
